6b39681a2c4f85d09344412d54d0c4561d928664
hyfrommer <yaeli0556727164@gmail.com>
2024-12-02 19:20:45 +0200

    Re-added all files to repository with updated .gitignore

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+    template: "HI",
+    selector: "app-root"
+})
+export class AppComponent{
+    x: number = 5;
+
+    calc(){
+        return 0;
+    }
+    constructor(){
+
+    }
+}
